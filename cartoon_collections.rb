9de11bb88d97a13dd98604828ed9eb24ff0a1711
Placido Wang <placidow@gmail.com>
2020-01-23 05:00:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,9 @@ def long_planeteer_calls(words)
   end
 end
 
-long_planeteer_calls(["ad", "adf", "dkhgjak"])
-
-def find_the_cheese# code an argument here
+def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
 end
