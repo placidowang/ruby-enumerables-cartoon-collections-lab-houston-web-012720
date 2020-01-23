6b6bd760a9b6f06738bc72b6a43a7f6a804fa0e6
Placido Wang <placidow@gmail.com>
2020-01-23 04:49:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,11 +10,11 @@ def summon_captain_planet(words)
 end
 
 def long_planeteer_calls(words)
-  if words.select { |word| word.length > 4}
-    return 
-  else
-    return false
-  end
+  p words.select { |word| word.length > 4}
+  #   return 
+  # else
+  #   return false
+  # end
 end
 
 def find_the_cheese# code an argument here
